86374dc07c70560db9bc0602c83ad8e5083e211e
Daniel Rydlewski <65160086+danielrydlewski@users.noreply.github.com>
2022-11-18 14:43:08 +0100

    Update site.pp

--- manifest/site.pp
+++ manifest/site.pp
@@ -1,7 +1,10 @@
 node 'localhost.localdomain' {
-  include role::master_server
   file { '/root/README':
     ensure => file,
     content => "Welcome to ${fqdn}\n",
+    owner   => 'root',
+  }
+  file {'/root/README':
+    owner => 'roor',
   }
 }
